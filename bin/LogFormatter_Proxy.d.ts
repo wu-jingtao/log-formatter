33729082f026e9b4f450806df055d2a8d663b1a0
wujingtao <601595686@qq.com>
2018-08-20 22:22:28 +0800

    更新了一下项目配置

--- bin/LogFormatter_Proxy.d.ts
+++
@@ -1,5 +0,0 @@
-import { LogFormatter } from "./LogFormatter";
-export interface LogFormatter_Proxy extends LogFormatter {
-    (...args: any[]): void;
-}
-//# sourceMappingURL=LogFormatter_Proxy.d.ts.map
--- bin/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { LogFormatter_Proxy } from "./LogFormatter_Proxy";
-declare const __LogFormatter_Proxy: LogFormatter_Proxy;
-export default __LogFormatter_Proxy;
-//# sourceMappingURL=index.d.ts.map
