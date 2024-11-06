dc51cc2cea399735d7d53ee6b52c0d737680bed0
WuJingTao - 吴劲韬 <601595686@qq.com>
2024-11-06 05:21:57 +0000

    优化编译配置

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,10 @@ const sufu = require('eslint-config-sufu');
 module.exports = [
     ...sufu['ts'],
     {
-        files: ['**/*.{ts,mts,cts,tsx}'],
-        languageOptions: {
-            parserOptions: {
-                project: './tsconfig.eslint.json',
-                projectService: false
-            }
-        },
         rules: {
+            /**
+             * 添加自定义规则
+             */
         }
     }
 ];
